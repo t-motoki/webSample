b15054e8a486a011bdadbfa809734bf05f698010
TakashiMotoki <t.motoki01@gmail.com>
2018-07-12 19:37:34 +0900

    first commit
    Webの勉強用のソースコード

---
+++ 03_php/02/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// コメント
+# コメント
+/*
+あ
+あ
+*/
+
+echo "hello from the TOP!";
+
+ ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <body>
+
+    <p>Hello World<?php echo "from PHP"; ?></p>
+
+  </body>
+</html>
